9612cb0e5f371e2ba76644e3043c1f33823dc29b
Lionel Brisseau <lbrisseau@gmail.com>
2021-05-11 12:08:03 +0200

    security.yaml & controller authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(Request $request, Security $security, AuthenticationUtils $helper): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+        
+        if ($security->isGranted('ROLE_ADMIN')){
+            return $this->redirectToRoute('');
+        } elseif ($security->isGranted('ROLE_USER')){
+            return $this->redirectToRoute('');
+        }
+
+        // get the login error if there is one
+        $error = $helper->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $helper->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
