a51a5034d3d0226ffd9a8a8bd94043db6f07f21b
OraneM <orane.masure@gmail.com>
2021-05-15 20:22:24 +0200

    on this version the scroll down logo works (with unfinished responsiveness)

--- public/js/home.js
+++ public/js/home.js
@@ -23,22 +23,22 @@ function initMap() {
 // Scroll icon function
 let el = document.querySelector(".scroll-container");
 el.addEventListener("scroll", function () {
-	console.log(window.screen.width);
+	// console.log(window.screen.width);
 	if (window.screen.width >= 925)
 	{
 		let scrollHeight = el.scrollHeight - el.clientHeight;
 		let currentHeight = el.scrollTop;
-
+		// console.log(scrollHeight+" "+currentHeight);
 		if (currentHeight > scrollHeight - 10)
 		{
-			document.querySelector('.my-scroll-down-arrow').style.display = 'none';
+			document.querySelector('.my-scroll-down-arrow').style.visibility = 'collapse';
 		} 
 		else
 		{
-			document.querySelector('.my-scroll-down-arrow').style.display = 'block';
+			document.querySelector('.my-scroll-down-arrow').style.visibility = 'visible';
 		}
 	}
 	else{
-		document.querySelector('.my-scroll-down-arrow').style.display = 'none';
+		document.querySelector('.my-scroll-down-arrow').style.visibility = 'collapse';
 	}
 });
