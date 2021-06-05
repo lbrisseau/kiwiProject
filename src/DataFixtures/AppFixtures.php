d8b614462702e491ca3947b9cd517097c66a4990
Lionel Brisseau <lbrisseau@gmail.com>
2021-06-05 19:54:27 +0200

    Subscriptions datas fictures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use App\Entity\Subscription;
 use App\Entity\User;
 use DateInterval;
 use DateTime;
@@ -22,6 +23,12 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        
+        $nbUsers = 100;
+        $nbEvents = 5;
+
+        // Creation d'un user Admin
+
         $user = new User;
         $faker = Faker\Factory::create('fr_FR');
 
@@ -33,7 +40,7 @@ class AppFixtures extends Fixture
         $user->setLastName($lastName);
         $user->setEmail($email);
 
-        //encodage et définition du mot de passe
+        //encodage et définition du mot de passe (=prenom)
         $password = $this->encoder->encodePassword($user, $firstName);
         $user->setPassword($password);
 
@@ -43,10 +50,45 @@ class AppFixtures extends Fixture
 
         $manager->persist($user);
         $manager->flush();
-        
+
+        // Alimentation de la table event
+
+        $tabEvent = []; //tableau des Event créés sera utile pour remplissage de la table subscription
+
+        $date = new DateTime();
+
+        for ($count = 0; $count < $nbEvents; $count++) {
+            $event = new Event;
+
+            $newDate = $date->add(new DateInterval('P7D'));
+
+            $event->setDate($newDate);
+            $event->setName($newDate->format('Y-m-d'));
+
+            if ($count & 1) {
+                // si $count est impair
+                $event->setType(true);
+                $event->setName("Entrainement Adulte du " . $newDate->format('d/m/Y'));
+            } else {
+                // si $count est pair
+                $event->setType(false);
+                $event->setName("Entrainement Kids du " . $newDate->format('d/m/Y'));
+            }
+
+            $event->setStartMemberSubs(21);
+            $event->setStartAllSubs(14);
+            $event->setEndSubs(2);
+
+            $manager->persist($event);
+            $tabEvent[] = $event;
+        }
+        $manager->flush();
+
         // Alimentation de la table user
 
-        for ($count = 0; $count < 40; $count++) {
+        $tabUser = []; //tableau des user créés sera utile pour remplissage de la table subscription
+
+        for ($count = 0; $count < $nbUsers; $count++) {
             $user = new User;
             $faker = Faker\Factory::create('fr_FR');
 
@@ -59,48 +101,34 @@ class AppFixtures extends Fixture
             $user->setLastName($lastName);
             $user->setEmail($email);
 
-            //encodage et définition du mot de passe
+            //encodage et définition du mot de passe (=prenom)
             $password = $this->encoder->encodePassword($user, $firstName);
             $user->setPassword($password);
 
             $user->setPhone($faker->phoneNumber());
             $user->setBirthDate($faker->dateTimeThisCentury());
             $user->setRoles(["ROLE_USER"]);
-
             $manager->persist($user);
+
+            $tabUser[] = $user;
         }
         $manager->flush();
 
-        // Alimentation de la table event
+        // Alimentation de la table Subscription
 
-        $date = new DateTime();
+        for ($count = 0; $count < $nbUsers; $count++) {
+            $sub = new Subscription;
+            $user = $tabUser[$count];
+            $nbEvent = count($tabEvent)-1;
+            $event = $tabEvent[rand(0,$nbEvent)];
+            $sub->setEvent($event);
+            $sub->setUser($user);
+            $sub->setSubsDate($date);
+            $sub->setValidationState(false);
 
-        for ($count = 0; $count < 10; $count++) {
-            $event = new Event;
-
-            $newDate = $date->add(new DateInterval('P7D'));
-            
-            $event->setDate($newDate);
-            $event->setName($newDate->format('Y-m-d'));
-
-            if ($count&1) {
-                // si $count est impair
-                $event->setType(true);
-                $event->setName("Entrainement Adulte du ".$newDate->format('d/m/Y'));
-            } else {
-                // si $count est pair
-                $event->setType(false);
-                $event->setName("Entrainement Kids du ".$newDate->format('d/m/Y'));
-            }
-            
-            $event->setStartMemberSubs(21);
-            $event->setStartAllSubs(14);
-            $event->setEndSubs(2);
-
-            $manager->persist($event);
-            $manager->flush();
+            $manager->persist($sub);
         }
 
-        
+        $manager->flush();
     }
 }
