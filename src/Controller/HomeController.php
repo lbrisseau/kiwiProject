af04cba482149021506cca7d99033536f1e7ddba
OraneM <orane.masure@gmail.com>
2021-06-09 08:59:27 +0200

    little cleaning with twig files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -35,39 +35,42 @@ class HomeController extends AbstractController
             $this->addFlash('success', "Votre message a bien été envoyé.");
             return $this->redirectToRoute("home", ['_fragment' => 'contact']);
         }
-        // Contact form management:
-        $subs = new Subscription();
-        $formSubs = $this->createForm(SubscriptionType::class, $subs);
-        $formSubs->handleRequest($request);
-        if ($formSubs->isSubmitted())
-        {
-            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-            $user = $this->getUser();
-            $subs->setSubsDate(new DateTime());
-            $subs->setValidationState(!is_null($user->getLicenceNumber()));
-            $adultDate = new DateTime('-18Y');
-            if ($user->getBirthDate() > $adultDate)
-            {
-                $subs->setEvent($eventRepo->findNextKid());
-            }
-            else
-            {
-                $subs->setEvent($eventRepo->findNext());
-            }
-            $subs->setUser($user);
-
-            $manager->persist($subs);
-            $manager->flush();
-
-            $this->addFlash('success', "Vous êtes inscrit.e à l'événement.");
-            return $this->redirectToRoute("home");
-        }
         // Normal rendering:
         return $this->render('home/index.html.twig', [
             'formContact' => $formContact->createView(),
-            'formSubs' => $formSubs->createView(),
             'event' => $eventRepo->findNext(),
             'kids' => $eventRepo->findNextKid()
         ]);
+    }
+
+    /**
+     * @Route("/subs", name="subscriptionManager")
+     */
+    public function subscriptionManager(Request $request, EventRepository $eventRepo, EntityManagerInterface $manager)
+    {
+        // New subscription management:
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        $subs = new Subscription();
+        $formSubs = $this->createForm(SubscriptionType::class, $subs);
+        $formSubs->handleRequest($request);
+        $subs->setSubsDate(new DateTime());
+        $subs->setValidationState(!is_null($user->getLicenceNumber()));
+        $adultDate = new DateTime('-18Y');
+        // var_dump($subs);
+        // exit;
+        if ($user->getBirthDate() > $adultDate)
+        {
+            $subs->setEvent($eventRepo->findNextKid()[0]);
+        }
+        else
+        {
+            $subs->setEvent($eventRepo->findNext()[0]);
+        }
+        $subs->setUser($user);
+        $manager->persist($subs);
+        $manager->flush();
+        $this->addFlash('success', "Vous êtes inscrit.e à l'événement.");
+        return $this->redirectToRoute("home");
     }
 }
